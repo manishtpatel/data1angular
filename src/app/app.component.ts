eee0fd4fee132faee3b072602035691208b7ed19
Manish Patel <manishtpatel@yahoo.com>
2020-03-15 19:52:31 -0400

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(
+    private _snackBar: MatSnackBar,
+    private _http: HttpClient,
+    public dialog: MatDialog
+  ) { }
+
   title = 'gstweb';
+  products;
+  selectedProduct;
+
+  async ngOnInit() {
+    this.products = await this._http.get('http://localhost:3000/getproducts').toPromise()
+  }
+
+  onClick_Add = async () => {
+    const dialogRef = this.dialog.open(ProductNameDialog, {
+      width: '250px',
+      data: { name: '', }
+    });
+
+    dialogRef.afterClosed().subscribe(async result => {
+      console.log('The dialog was closed', result);
+
+      if (result) {
+        await this._http.post('http://localhost:3000/addproduct', { name: result }).toPromise()
+        this.products = await this._http.get('http://localhost:3000/getproducts').toPromise()
+
+        this._snackBar.open(`${result} has been created.`, null, { duration: 2000 });
+        this.selectedProduct = this.products[0]._id
+      }
+    });
+  }
+
+  onSelectionChanged_Product = async () => {
+    console.log(this.selectedProduct)
+  }
+}
+
+export interface DialogData {
+  name: string;
+}
+
+
+@Component({
+  selector: 'product-name-dialog',
+  templateUrl: 'product-name-dialog.html',
+})
+export class ProductNameDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<ProductNameDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, ProductNameDialog } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductNameDialog,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatDialogModule,
+    FormsModule,
+    MatInputModule,
+  ],
+  entryComponents: [
+    ProductNameDialog
   ],
   providers: [],
   bootstrap: [AppComponent]
