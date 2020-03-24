3aaa160ee9bca3aabe62522ced1bf8c53a34a7fa
Manish Patel <manishtpatel@yahoo.com>
2020-03-23 23:34:48 -0400

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -19,6 +18,8 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
 
 import { ProcessComponent } from './process/process.component';
 
@@ -46,6 +47,7 @@ import { ProcessComponent } from './process/process.component';
     ReactiveFormsModule,
     MatInputModule,
     MatCheckboxModule,
+    MatChipsModule,
   ],
   entryComponents: [
     ProductNameDialog
--- src/app/process/process.component.ts
+++ src/app/process/process.component.ts
@@ -10,8 +10,12 @@ import { MatStepper } from '@angular/material/stepper'
 })
 export class ProcessComponent implements OnInit, OnChanges, AfterViewInit {
   firstFormGroup: FormGroup;
+  step1FormControl: FormControl;
+  step2FormControl: FormControl;
+  step3FormControl: FormControl;
+  step4FormControl: FormControl;
   secondFormGroup: FormGroup;
-  interval
+  interval: any;
 
   @Input() product
   @ViewChild('stepper') myStepper: MatStepper;
@@ -23,59 +27,80 @@ export class ProcessComponent implements OnInit, OnChanges, AfterViewInit {
   ) { }
 
   ngAfterViewInit(): void {
-    this.onUpdatedProduct();
+    // this.onUpdatedProduct(true);
 
-    this.interval = setInterval(() => {
-      this.refreshData();
-    }, 5000);
+    // this.interval = setInterval(() => {
+    //   this.refreshData();
+    // }, 5000);
   }
 
   ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
     if (changes.product.firstChange) return;
 
-    this.onUpdatedProduct();
+    this.onUpdatedProduct(true);
   }
 
   ngOnInit(): void {
+    this.step1FormControl = new FormControl(this.product.state, Validators.min(2))
+    this.step2FormControl = new FormControl(this.product.state, Validators.min(3))
+    this.step3FormControl = new FormControl(this.product.state, Validators.min(4))
+    this.step4FormControl = new FormControl(this.product.state, Validators.min(5))
+
     this.firstFormGroup = this._formBuilder.group({
       testcsv1: new FormControl(null, Validators.required),
       testcsv2: new FormControl(null, Validators.required),
-      testcsv3: new FormControl(null, Validators.required),
     });
 
     this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['']
+      selectedSaleConditions: new FormControl(null, Validators.required)
     });
+
+        this.interval = setInterval(() => {
+      this.refreshData();
+    }, 5000);
   }
 
   refreshData() {
     this._http.get(`http://localhost:3000/getproduct/${this.product._id}`).subscribe(value => {
       this.zone.run(() => {
+        const isStateChanged = (value[0].state != this.product.state)
         this.product = value[0]
 
-        this.onUpdatedProduct()
+        this.onUpdatedProduct(isStateChanged)
       })
     })
   }
 
-  onUpdatedProduct() {
+  onUpdatedProduct(isStateChanged: boolean) {
     console.log('onUpdatedProduct', this.product)
 
-    if (this.product.state > 1) {
-      this.firstFormGroup.disable();
-    } else {
-      this.firstFormGroup.enable();
-    }
+    this.step1FormControl.setValue(this.product.state)
+    this.step2FormControl.setValue(this.product.state)
+    this.step3FormControl.setValue(this.product.state)
+    this.step4FormControl.setValue(this.product.state)
 
-    // set stepper step
-    if (this.myStepper) {
+    if (isStateChanged) {
       this.myStepper.reset();
+
+      // some woodo magic with step selection, to allow linear mode skip selection, mark all as completed
+      for (let index = 0; index < Math.floor(this.product.state) - 1; index++) {
+        this.myStepper.steps.forEach((item, iindex, steps) => {
+          if (iindex == index) {
+            item.completed = true;
+            item.interacted = false;
+          }
+        })
+      }
+
       this.myStepper.selectedIndex = Math.floor(this.product.state) - 1
     }
   }
 
   async onClick_step1_import() {
-    let response1 = await this._http.post(`http://localhost:3000/updatestatus/`, {
+    // intermittent for ui to reflect progress bar
+    this.product.state = 1.1
+
+    await this._http.post(`http://localhost:3000/updatestatus/`, {
       productId: this.product._id,
       state: 1.1,
     }).toPromise()
@@ -88,5 +113,20 @@ export class ProcessComponent implements OnInit, OnChanges, AfterViewInit {
     }).toPromise()
 
     await this._http.get('http://localhost:3000/proxy/api/experimental/dags/import_data/dag_runs/' + response2['execution_date']).toPromise()
+  }
+
+  async onClick_step2_done(state: number) {
+    // intermittent for ui to reflect progress bar
+    this.product.state = state
+
+    await this._http.post(`http://localhost:3000/updatestatus/`, {
+      productId: this.product._id,
+      state: Math.ceil(state),
+    }).toPromise()
+  }
+
+  getColor(isState: number){
+    if(this.product.state >= isState) return "primary"
+    else return ""
   }
 }
